2eaf63d5ab404b5a45865b29fe91f436821e8fad
rushiM2201 <rushikeshmitech@gmail.com>
2025-10-02 19:25:55 +0530

    enter space

--- app/Config/App.php
+++ app/Config/App.php
@@ -17,6 +17,7 @@ class App extends BaseConfig
      * E.g., http://example.com/
      */
     public string $baseURL = 'http://localhost/mobile_services_app';
+    
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
